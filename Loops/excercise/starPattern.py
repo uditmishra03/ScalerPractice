81da2836d44739d7918733698752c95df68c7be7
Gitea <gitea@fake.local>
2024-05-14 21:49:44 +0530

    excercise problems of loops-set 4

--- Loops/excercise/starPattern.py
+++ Loops/excercise/starPattern.py
@@ -11,3 +11,19 @@ Write a program to print the following start pattern using the for loop
 * *
 *'''
 
+num = 5
+for i in range(1, num+1):
+    for j in range(i):
+        if j == i-1:
+            print('*', end ='')
+        else:
+            print('*', end=' ')
+    print()
+for i in range(num-1, 0, -1):
+    for j in range(i):
+        if j == i - 1:
+            print('*', end = '')
+        else:
+            print('*', end=' ')
+    if i !=1:
+        print()
