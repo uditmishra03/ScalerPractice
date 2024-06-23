1abca8991cde0f9c2da74fcdfaeecf1c41804e25
uditmishra <uditmishra.um@gmail.com>
2024-06-23 17:40:20 +0530

    Interview questions: Added problem statement for n/3 repeated numbers

--- DSA/interviewQuestions/nBy3repeatNumber.py
+++ DSA/interviewQuestions/nBy3repeatNumber.py
@@ -1,3 +1,11 @@
+'''Problem Description
+You're given a read-only array of N integers. Find out if any integer occurs more than N/3 times in the array in linear time and constant additional space.
+If so, return the integer. If not, return -1.
+
+If there are multiple solutions, return any one.
+
+Note: Read-only array means that the input array should not be modified in the process of solving the problem'''
+
 # A = [1,2,3 ,1 ,1]
 # A=[ 1, 1, 1, 2, 3, 5, 7 ]
 
