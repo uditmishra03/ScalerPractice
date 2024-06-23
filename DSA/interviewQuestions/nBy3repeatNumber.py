4d89792c66f217b6451000dca3b5314f3bd5b0e3
uditmishra <uditmishra.um@gmail.com>
2024-06-23 17:43:01 +0530

    Interview questions: n/3 repeat numbers code improvements.

--- DSA/interviewQuestions/nBy3repeatNumber.py
+++ DSA/interviewQuestions/nBy3repeatNumber.py
@@ -6,26 +6,29 @@ If there are multiple solutions, return any one.
 
 Note: Read-only array means that the input array should not be modified in the process of solving the problem'''
 
-# A = [1,2,3 ,1 ,1]
+A = [1,2,3 ,1 ,1]
 # A=[ 1, 1, 1, 2, 3, 5, 7 ]
+# A= [1, 2, 3]
 
-A= [1, 2, 3]
-A.sort()
-print(A)
-element = A[0]
-# count = 1
-n = len(A)
+arr = A
+arr.sort()
+
+print(arr)
+
+element = arr[0]
+
+n = len(arr)
 threshold = n//3
 print("threshold: ", threshold)
 
 element_count = dict()
-element_count[A[0]] = 1
+element_count[arr[0]] = 1
 # print(element_count)
 for i in range(1, n):
-    if A[i] == A[i-1]:
-        element_count[A[i]] = element_count[A[i-1]] + 1
+    if arr[i] == arr[i - 1]:
+        element_count[arr[i]] = element_count[arr[i - 1]] + 1
     else:
-        element_count[A[i]] = 1
+        element_count[arr[i]] = 1
 
 print(element_count)
 keys = element_count.keys()
