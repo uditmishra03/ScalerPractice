c11a4efa4e4d9c152b7f5c3d64dc646c7ed578d2
uditmishra <uditmishra.um@gmail.com>
2024-06-26 21:02:22 +0530

    [DSA][1D Array]: Flip Binary values

--- DSA/1D_array/flipping_binaryValues.py
+++ DSA/1D_array/flipping_binaryValues.py
@@ -6,36 +6,37 @@ Your aim is to perform ATMOST one operation such that in the final string number
 If you don't want to perform the operation, return an empty array. Else, return an array consisting of two elements denoting L and R. If there are multiple solutions, return the lexicographically smallest pair of L and R.
 
 NOTE: Pair (a, b) is lexicographically smaller than pair (c, d) if a < c or, if a == c and b < d'''
+import sys
 
-A = "010"
-n = len(A)
+A = "111"
+
 print('Original String: ', A)
 
-# function to count 1's
-def countOnes(binaryString):
-    oneCount = 0
-    # print(binaryString[left:right])
-    for i in binaryString:
-        if i == '1':
-            oneCount += 1
-    return oneCount
+n = len(A)
+l, r , cSum, maxSum = 0, 0, 0, 0
+ans=[0]*2
 
-# finding the prefix for count of 1s
-pf_OneCount = [0]*n
-if A[0] == '1':
-    pf_OneCount[0] = 1
-else:
-    pf_OneCount[0] = 0
-for i in range(1, n):
+for i in range(n):
     if A[i] == '1':
-        pf_OneCount[i] = pf_OneCount[i-1] +1
-    else:
-        pf_OneCount[i] = pf_OneCount[i-1]
+        cSum -=1
+    if A[i] == '0':
+        cSum +=1
 
-print(pf_OneCount)
-# print(countOnes(A))
-ans = 0
-indexes = []
-for i in range(0, n+1):
-    for j in range(i, n+1):
-        print(f"i: {i}, j: {j}, string: {A[i:j+1]}, Count of Ones: , {countOnes(A[i:j+1])}" )
+    if cSum > maxSum:
+        maxSum = cSum
+        ans[0] = l+1
+        ans[1] = r+1
+
+    if cSum < 0:
+        cSum = 0
+        l = i+1
+        r = i+1
+    else:
+        r +=1
+
+if maxSum == 0:
+    noans = []
+    print(noans)
+    # print('No solution found')
+else:
+    print(ans)
