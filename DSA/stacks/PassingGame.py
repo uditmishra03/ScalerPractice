8fe9e2a7f33d5668f7617afc208f414e31c69828
uditmishra03 <uditmishra.um@gmail.com>
2024-08-13 23:08:43 +0530

    [DSA][Stacks]: Assignment problems-set2

--- DSA/stacks/PassingGame.py
+++ DSA/stacks/PassingGame.py
@@ -1,3 +1,22 @@
+'''Problem Description
+There is a football event going on in your city. In this event, you are given A passes and players having ids between 1 and 106.
+
+Initially, some player with a given id had the ball in his possession. You have to make a program to display the id of the player who possessed the ball after exactly A passes.
+
+There are two kinds of passes:
+
+1) ID
+
+2) 0
+
+For the first kind of pass, the player in possession of the ball passes the ball "forward" to the player with id = ID.
+
+For the second kind of pass, the player in possession of the ball passes the ball back to the player who had forwarded the ball to him.
+
+In the second kind of pass "0" just means Back Pass.
+
+Return the ID of the player who currently possesses the ball.'''
+
 # A = 10
 # B = 23
 # C = [86, 63, 60, 0, 47, 0, 99, 9, 0, 0]
